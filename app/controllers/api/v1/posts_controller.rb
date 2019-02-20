702cb33069b73fd38597136eb8019af641e03d19
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-20 15:35:38 +0300

    add create user

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+
+
+    #GET /posts
+    def index
+        @posts = Post.all
+        json_response(@posts)
+    end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,39 @@
+module Api
+    module V1
+        class UsersController < ApplicationController
+        
+            def index
+                @users = User.order('created_at DESC')
+                render json: {data:{
+                    type: 'user',
+                    attributes: {
+                        data:@user 
+                    }
+                }}, status: :ok
+            end
+
+            def create
+                @user = User.new(user_params)
+                if @user.save
+                    render json: {data:{
+                    type: 'user',
+                    attributes: {
+                        data:@user 
+                    }
+                }}, status: :created_at
+                else
+                    render json: {errors:[
+                                data:@user.errors.each
+                            ]
+                        }
+                end
+            end
+
+            private
+
+            def user_params
+                params.permit(:username, :password, :full_name)
+            end
+        end
+    end
+end
