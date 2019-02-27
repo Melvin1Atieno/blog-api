06aed622a0a8dc3722365a5c436c523e17062976
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-27 18:59:22 +0300

    Add basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :comments
     has_many :posts
     validates :username, :password, :full_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post 'authenticate', to: 'authentication#authenticate'
       resources :users, only: [:create] do |users|
         resources :posts, :name_prefix => "user_"
       end
