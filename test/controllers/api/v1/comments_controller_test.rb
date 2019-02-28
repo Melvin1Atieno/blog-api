fdffe854b5aa40593e4b991940491ac4e2b95388
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-28 20:18:11 +0300

    add authentication credentials to tests

--- test/controllers/api/v1/comments_controller_test.rb
+++ test/controllers/api/v1/comments_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class Api::V1::CommentsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
+    @token = authenticated_user
+    @valid_user = User.find_by_username("valid_user")
     @comment = comments(:one)
     @post = posts(:one)
-    @user = users(:one)
   end
 
   teardown do
@@ -13,39 +14,45 @@ class Api::V1::CommentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Should get all comments associated with a post" do
-    get api_v1_post_comments_path(@post.id)
+    get api_v1_post_comments_path(@post.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
     assert_response 200
   end
 
   test "Should be able to get all comments associated with user" do
-    get api_v1_user_comments_path(user_id:@user.id)
+    get api_v1_user_comments_path(user_id:@valid_user.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
     assert_response 200
   end
 
   test "should be able to create a comment for a specific post" do
     assert_difference('Comment.count') do
-      post api_v1_post_comments_path(user_id:@user.id, post_id:@post.id),
+      post api_v1_post_comments_path(user_id:@valid_user.id, post_id:@post.id),
       params: {
         body:"This is a new comment"
       },
-      headers: {"Accept": "Application/json"}
+      headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+      # binding.pry
     end
     assert_response 201
   end
   
   test "Should be able to get a single comment" do
-    get api_v1_comment_path(id:@comment.id)
+    get api_v1_comment_path(id:@comment.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
     assert_response 200
   end
 
   test "should be able to update a specific comment" do
-    patch api_v1_comment_path(id:@comment.id)
+    patch api_v1_comment_path(id:@comment.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
     assert_response 200
   end
 
   test "should delete a specific comment" do
     assert_difference('Comment.count', -1) do
-    delete api_v1_comment_path(user_id:@user.id,id:@comment.id)
+    delete api_v1_comment_path(user_id:@valid_user.id,id:@comment.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
     end
     assert_response 204
   end
--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -13,11 +13,11 @@ class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
     Rails.cache.clear
   end
  
-  # test "Should return all posts" do	
-  #     get api_v1_posts_path,
-  #     headers: {"Accept": "Application/json", "Authorization": @valid_token["auth_token"]}
-  #     assert_response 200
-  #   end
+  test "Should return all posts" do	
+      get api_v1_posts_path,
+      headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+      assert_response 200
+    end
     
     test "Should create a new post" do
       assert_difference('Post.count') do
@@ -27,51 +27,52 @@ class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
           text: @post.text
         },
         headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
-        binding.pry
     end
     assert_response 201
   end
 
-  # test "should get post by id" do
-  #   get api_v1_post_path(@post.id)
-  #   assert_response 200
-  # end
+  test "should get post by id" do
+    get api_v1_post_path(@post.id),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+    assert_response 200
+  end
 
-  # test "Should return error if post with given id does not exist" do
-  #   get api_v1_post_url(id: 1234)
-  #   assert_response 404
-  # end
+  test "Should return error if post with given id does not exist" do
+    get api_v1_post_url(id: 1234),
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+    assert_response 404
+  end
 
-  # test "Should be able to update single post" do
-  #     patch api_v1_post_path(@post.id),
-  #     params: { 
-  #       title: "updated title",
-  #       text: "updated text"
-  #     },
-  #   headers: {"Accept": "Application/json"}
-  #   @post.reload
-  #   assert_response 200
-  #   assert_equal "updated title", @post.title 
-  #   assert_equal "updated text", @post.text 
-  # end
+  test "Should be able to update single post" do
+      patch api_v1_post_path(@post.id),
+      params: { 
+        title: "updated title",
+        text: "updated text"
+      },
+    headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+    @post.reload
+    assert_response 200
+    assert_equal "updated title", @post.title 
+    assert_equal "updated text", @post.text 
+  end
 
-  # test "Should return error if requested post for update does not exist" do
-  #   patch api_v1_post_path(1234),
-  #   params: { 
-  #     title: "updated title",
-  #     text: "updated text"
-  #   },
-  # headers: {"Accept": "Application/json"}
-  #   assert_response 404
-  # end
+  test "Should return error if requested post for update does not exist" do
+    patch api_v1_post_path(1234),
+    params: { 
+      title: "updated title",
+      text: "updated text"
+    },
+  headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+    assert_response 404
+  end
 
-  # test "Should delete post" do
-  #   assert_difference('Post.count', -1) do
-  #     delete api_v1_post_path(id:@post.id)
-  #     # binding.pry
-  #   end
-  #   assert_response 204
-  # end
+  test "Should delete post" do
+    assert_difference('Post.count', -1) do
+      delete api_v1_post_path(id:@post.id),
+      headers: {"Accept": "Application/json", "Authorization": @token["auth_token"]}
+    end
+    assert_response 204
+  end
  
 
 end
