299eb41aa5b49422f19c3b1ee39afa6fe1a485f1
Melvin1Atieno <melvinatieno@gmail.com>
2019-03-01 11:43:39 +0300

    update to require jwt

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require 'jwt'
 class AuthenticateUser
     prepend SimpleCommand
 
@@ -9,7 +9,6 @@ class AuthenticateUser
     end
 
     def call
-
         JsonWebToken.encode(user_id: user.id) if user
     end
 
