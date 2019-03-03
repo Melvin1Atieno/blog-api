f7833fb4263aab2ff2256d4a036c73417f7c0885
Melvin1Atieno <melvinatieno@gmail.com>
2019-03-03 15:34:26 +0300

    setup linting

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,16 +1,11 @@
 class Api::V1::AuthenticationController < ApplicationController
-    skip_before_action :authenticate_request
-
-    def authenticate
-        # binding.pry
-        command = AuthenticateUser.call(params[:username], params[:password])
-
-        if command.success?
-            render json: {auth_token: command.result}, status: 200
-        else
-            render json: {error: command.errors}, status: :unauthorized
-        end
+  skip_before_action :authenticate_request
+  def authenticate
+    command = AuthenticateUser.call(params[:username], params[:password])
+    if command.success?
+      render json: { auth_token: command.result }, status: 200
+    else
+      render json: { error: command.errors }, status: :unauthorized
     end
-
-    private 
+  end
 end
--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,107 +1,108 @@
+# comments
 module Api
-    module V1
-        class CommentsController < ApplicationController
+  module V1
+    class CommentsController < ApplicationController
+      def index
+        @comments = Comment.all
+        render json:{
+                      data:{ 
+                              type: 'comment',	
+                              attributes:{ data:@comments }
+                           }
+                    },
+               status: 200
+      end
 
-            def index
-                @comments = Comment.all
-                render json: {data:{	
-                    type: 'comment',	
-                    attributes: {	
-                        data:@comments 	
-                    }	
-                }}, status: 200
-                    
-            end
-
-            def create 
-                @post = Post.find(comment_params[:post_id])
-                @user = User.find(comment_params[:user_id])
-                @new_comment = Comment.new(comment_params)
-                @post.comments << @new_comment
-                @user.comments << @new_comment
-                if @new_comment.save
-                    render json: {data:{
-                            type: 'post',
-                            attributes: {
-                                commenter:@user.username,
-                                body: @new_comment.body,
-                                post_id: @post.id
-                            }
-                        }}, status: 201
-                else
-                    render json: { 
-                        errors:[
-                        {
-                            status: "400",
-                            title: "Bad request",
-                            detail: @new_comment.errors
-                        }]
-
-                    }, status: 400
-                end
-            end
-            def destroy
-                if Comment.find_by_id(params[:id]).nil?
-                    render json: { 
-                       data: "null"
-                    }, status: 404
-                else
-                    @comment = Comment.find(params[:id])
-                    @comment.destroy
-                    render json: {data:{
-                            type: 'comment',
-                            attributes: {
-                                data: "deleted"
-                            }
-                        }}, status: 204
-                end
-            end
-
-            def show
-                if Comment.find_by_id(params[:id]).nil?
-                    render json: { 
-                       data: "null"
-
-                    }, status: 404
-
-                else
-                  @comment = Comment.find(params[:id])
-                  render json: {data:{
-                    type: 'post',
-                    id: @comment.id,
-                    attributes: {
-                        commenter_id: @comment.user_id,
-                        body: @comment.body,
-                        post_id: @comment.post_id
-                    }
-                }}, status: 200
-                end
-            end
-
-            def update
-                if Comment.find_by_id(params[:id]).nil?
-                    render json: { 
-                       data: "null"
-                    }, status: 404
-                else
-                    @comment = Comment.find(params[:id])
-                    @updated_Comment = @comment.update(comment_params)
-                    render json: {data:{
-                        type: 'post',
-                        attributes: {
-                            data: @updated_comment
-                        }
-                    }}, status: 200
-                end
-            end
-
-            private
-
-            def comment_params
-                params.permit(:body,:post_id,:user_id)
-            end
-
+      def create 
+        @post = Post.find(comment_params[:post_id])
+        @user = User.find(comment_params[:user_id])
+        @new_comment = Comment.new(comment_params)
+        @post.comments << @new_comment
+        @user.comments << @new_comment
+        if @new_comment.save
+          render json:{
+                        data:{
+                               type: 'post',
+                               attributes:{
+                                            commenter:@user.username,
+                                            body: @new_comment.body,
+                                            post_id: @post.id
+                                          }
+                             }
+                      }, status: 201
+        else render json:{ 
+                           errors:[{
+                                     status: "400",
+                                     title: "Bad request",
+                                     detail: @new_comment.errors
+                                    }]
+                         }, status: 400
         end
-        
+      end
+      def destroy 
+        if Comment.find_by_id(params[:id]).nil?
+          render json:{ 
+                        data: "null"
+                      },
+                 status: 404
+        else
+          @comment = Comment.find(params[:id])
+          @comment.destroy
+          render json: {data:{
+                               type: 'comment',
+                               attributes: {
+                                            data: "deleted"
+                                           }
+                             }
+                       }, status: 204
+        end
+      end
+
+      def show
+        if Comment.find_by_id(params[:id]).nil?
+          render json: { 
+                         data: "null"
+                       }, 
+                 status: 404
+        else
+          @comment = Comment.find(params[:id])
+          render json:{ 
+                        data:{
+                              type:'post',
+                              id: @comment.id,
+                              attributes: {
+                                            commenter_id: @comment.user_id,
+                                            body: @comment.body,
+                                            post_id: @comment.post_id
+                                          }
+                              }
+                      }, status: 200
+        end
+      end
+
+      def update
+        if Comment.find_by_id(params[:id]).nil?
+          render json:{ 
+                       data: "null"
+                      }, 
+                 status: 404
+        else
+            @comment = Comment.find(params[:id])
+            @updated_Comment = @comment.update(comment_params)
+            render json: {data:{
+                type: 'post',
+                attributes: {
+                    data: @updated_comment
+                }
+            }}, status: 200
+        end
+        end
+
+    private
+      def comment_params
+        params.permit(:body,:post_id,:user_id)
+      end
+
     end
+  end
 end
